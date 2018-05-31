a8d4cbcfe3d8ff397589ff128f8e0e2d193c075a
Ryan Namgung <heon.namgung@richmond.edu>
2018-05-31 23:00:15 +0000

    automatically backed up by learn

--- spec/meal_choice_spec.rb
+++ spec/meal_choice_spec.rb
@@ -1,4 +1,4 @@
-describe '#meal_choice' = 'meat' do
+describe '#meal_choice' do
   it 'should default to meat' do
     expect(meal_choice).to eq("meat")
   end
@@ -6,4 +6,8 @@ describe '#meal_choice' = 'meat' do
   it 'should allow you to set a meal' do
     expect(meal_choice("vegan")).to eq("vegan")
   end
+end
+
+def meal_choice(meal_choice = "meat")
+  return meal_choice
 end
